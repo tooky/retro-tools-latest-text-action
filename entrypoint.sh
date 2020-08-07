629a80de72b81274779a8d9f1b1c119533517db2
Julien Biezemans <jb@jbpros.com>
2020-08-07 15:00:54 +0100

    Fix bug where directory listing was appearing in the output

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,9 @@ content="${1}"
 content="${content//'%'/'%25'}"
 content="${content//$'\n'/'%0A'}"
 content="${content//$'\r'/'%0D'}"
-echo $content
+echo "$content"
 }
 
-text=$(retro latest)
+cd $GITHUB_WORKSPACE
+text="$(retro latest)"
 echo "::set-output name=text::$(escape_output "${text}")"
